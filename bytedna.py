dd5f086fa0c722e295e7e51cb9abaa05684166fa
Tuizku <tuisku.kahra@gmail.com>
2024-09-24 19:13:57 +0300

    bytedna.py is ready for use

--- bytedna.py
+++ bytedna.py
@@ -216,4 +216,235 @@ class ByteDNA:
 
         return result
 
-    # more functions...
+    def get_separated_genome(self, genome : bytearray):
+        return [genome[i : i + self.gene_bytes] for i in range(0, len(genome), self.gene_bytes)]
+
+    def get_all_neurons(self):
+        neurons_inputs = []
+        neurons_output = []
+        neurons_function = []
+
+        # Add inputs
+        for input_func in self.inputs:
+            neurons_inputs.append([])
+            neurons_output.append(0)
+            neurons_function.append(input_func)
+
+        # Add inner neurons
+        for i in range(self.inner_neurons):
+            neurons_inputs.append([])
+            neurons_output.append(0)
+            neurons_function.append(None)
+        
+        # Add outputs
+        for output_func in self.outputs:
+            neurons_inputs.append([])
+            neurons_output.append(0)
+            neurons_function.append(output_func)
+        
+        return neurons_inputs, neurons_output, neurons_function
+
+    def get_needed_neurons(self, genome : bytearray):
+        """
+        Gets only the needed neurons for the creature.
+
+        Returns
+        -------
+            neurons_inputs (list): Every neuron's current inputs.
+            neurons_output (list): Every neuron's current output.
+            neurons_function (list): Every neuron's unique function. Inner neurons have None.
+            include_dictionary (dict): Boolean numpy lists. Which neurons are used? ("include_inputs", "include_outputs", "include_inners")
+        """
+
+        # Include neurons
+        # Creates lists of bools, True meaning that it will be added to the neuron list.
+        include_inputs = np.full(len(self.inputs), False, dtype=bool)
+        include_outputs = np.full(len(self.outputs), False, dtype=bool)
+        include_inners = np.full(self.inner_neurons, False, dtype=bool)
+
+        # Includes neurons in the result.
+        def include_neuron(is_source : bool, type : int, id : int):
+            nonlocal include_inputs, include_outputs, include_inners
+            if type == 1:
+                include_inners[id] = True
+            elif type == 0:
+                if is_source == True:
+                    include_inputs[id] = True
+                else:
+                    include_outputs[id] = True
+
+
+        # Include all neurons that are referenced in genome
+        decoded_genome = self.decode_genomes(genome, True)[0]
+        for decoded_gene in decoded_genome:
+            include_neuron(True, decoded_gene["source_type"], decoded_gene["source_id"])
+            include_neuron(False, decoded_gene["sink_type"], decoded_gene["sink_id"])
+        
+
+        # Result lists
+        neurons_inputs = []
+        neurons_output = []
+        neurons_function = []
+
+        # Add the included neurons in their correct order to the result lists
+        all_includes = [include_inputs, include_inners, include_outputs]
+        all_funcs = [self.inputs, np.full(len(include_inners), None), self.outputs]
+        for i in range(3):
+            for j in range(len(all_includes[i])):
+                if all_includes[i][j] == True:
+                    neurons_inputs.append([])
+                    neurons_output.append(0)
+                    neurons_function.append(all_funcs[i][j])
+
+
+        # Return 3 lists and a dict
+        return neurons_inputs, neurons_output, neurons_function, {
+            "include_inputs": include_inputs,
+            "include_outputs": include_outputs,
+            "include_inners": include_inners
+        }
+
+    def get_optimized_genome(self, genome : bytearray):
+        """
+        Cuts out inner neurons that don't have a valid source and sink. The genes that use these useless inner neurons, are cut out.
+        - A valid source is an input neuron or a different inner neuron.
+        - A valid sink is an output neuron or a different inner neuron.
+
+        Returns
+        -------
+            optimized_genome (list): Same data type as normal genome, but with possibly fewer genes.
+        """
+        
+        # Decode the whole genome with reranged values
+        decoded_genome = self.decode_genomes(genome, True)[0]
+        separated_genome = self.get_separated_genome(genome)
+
+        # 2 requirements for each inner neuron.
+        inner_neurons_requirements = np.full((self.inner_neurons, 2), fill_value=False, dtype=bool)
+
+        # Check inner neuron requirements from every gene.
+        for decoded_gene in decoded_genome:
+            source_type = decoded_gene["source_type"]
+            source_id = decoded_gene["source_id"]
+            sink_type = decoded_gene["sink_type"]
+            sink_id = decoded_gene["sink_id"]
+
+            # Case -> Sink = Inner neuron
+            if sink_type == 1:
+                if source_type == 0 or source_id != sink_id:
+                    inner_neurons_requirements[sink_id][0] = True
+            
+            # Case -> Source = Inner neuron
+            if source_type == 1:
+                if sink_type == 0 or sink_id != source_id:
+                    inner_neurons_requirements[source_id][1] = True
+        
+        # Turn requirements into booleans, which inner neurons will be used?
+        # Index in list = which inner neuron?
+        # Value is True if both requirements are True.
+        inner_neurons_included = []
+        for requirements in inner_neurons_requirements:
+            if requirements[0] == True and requirements[1] == True:
+                inner_neurons_included.append(True)
+            else: inner_neurons_included.append(False)
+        
+        # Adds only the needed genes to the result
+        optimized_genome = []
+        for i in range(len(separated_genome)):
+            decoded_gene = decoded_genome[i]
+            source_type = decoded_gene["source_type"]
+            source_id = decoded_gene["source_id"]
+            sink_type = decoded_gene["sink_type"]
+            sink_id = decoded_gene["sink_id"]
+
+            # Do not include gene if a source or a sink is an inner neuron that isn't required in the brain.
+            include_gene = True
+            if source_type == 1 and inner_neurons_included[source_id] == False:
+                include_gene = False
+            elif sink_type == 1 and inner_neurons_included[sink_id] == False:
+                include_gene = False
+            
+            # Add gene if included
+            if include_gene == True: optimized_genome.append(separated_genome[i])
+        
+        return optimized_genome
+
+    def genome_to_conns(self, genome : bytearray, include_dict : dict = None):
+        """Turns a genome into conns lists."""
+        
+        def set_tweaks(length, include_list, tweaks):
+            tweak = 0
+            for i in range(length):
+                if include_list[i] == False: tweak += 1
+                tweaks[i] = tweak
+            return tweak
+        
+        # Conns variables
+        conns_source_id = []
+        conns_sink_id = []
+        conns_weight = []
+
+        # Lengths
+        ins = len(self.inputs)
+        inners = self.inner_neurons
+        outs = len(self.outputs)
+        actual_ins = ins
+        actual_inners = inners
+
+        # Create the tweak lists. These values will be summed with source/sink ids.
+        # Their point is to transform normal ids to ids that reference actual neurons included in the creature.
+        input_tweaks = np.zeros(ins, dtype=int)
+        inner_tweaks = np.zeros(inners, dtype=int)
+        output_tweaks = np.zeros(outs, dtype=int)
+        if include_dict != None:
+            # Set the tweaks
+            actual_ins -= set_tweaks(ins, include_dict["include_inputs"], input_tweaks)
+            actual_inners -= set_tweaks(inners, include_dict["include_inners"], inner_tweaks)
+            set_tweaks(outs, include_dict["include_outputs"], output_tweaks)
+        
+
+        # Loop through all genes and turn them into conns
+        decoded_genome = self.decode_genomes(genome, True)[0]
+        for decoded_gene in decoded_genome:
+            source_id = decoded_gene["source_id"]
+            sink_id = decoded_gene["sink_id"]
+
+            # Tweak the source_id and sink_id
+            if decoded_gene["source_type"] == 0:
+                source_id -= input_tweaks[source_id]
+            else:
+                source_id -= inner_tweaks[source_id]
+
+            if decoded_gene["sink_type"] == 0:
+                sink_id -= output_tweaks[sink_id]
+            else:
+                sink_id -= inner_tweaks[sink_id]
+
+            # Turn source- and sink id's into combined ids
+            source_id += (decoded_gene["source_type"] * actual_ins)
+            sink_id += actual_ins + (actual_inners * (1 - decoded_gene["sink_type"]))
+            
+            conns_source_id.append(source_id)
+            conns_sink_id.append(sink_id)
+            conns_weight.append(decoded_gene["weight"])
+        
+        return conns_source_id, conns_sink_id, conns_weight
+
+    def average_hamming_distance(self, genomes : bytearray, population : int):
+        """
+        Calculates the average hamming distance between the genomes.
+        This can be used to see how different dna the creatures have.
+        """
+
+        diverse_bits = 0
+        int_genomes = [int.from_bytes(genomes[i * self.genome_len : i * self.genome_len + self.genome_len]) for i in range(population)]
+
+        for i in range(0, population - 1):
+            for j in range(1, population):
+                diverse_bits += (int_genomes[i] ^ int_genomes[j]).bit_count()
+        
+        all_bits = self.gene_bits * self.genome_len * population
+        result = diverse_bits / (all_bits * 0.5)
+
+        return result
+
